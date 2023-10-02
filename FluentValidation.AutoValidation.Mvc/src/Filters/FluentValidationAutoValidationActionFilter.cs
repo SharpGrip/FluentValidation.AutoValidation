84120430b4b1e791acc355bc94c27f301f6fea49
JonAdamsFromNC <jonadamsfromnc@gmail.com>
2023-10-02 09:39:08 -0400

    enable optional validation on custom binding source models

--- FluentValidation.AutoValidation.Mvc/src/Filters/FluentValidationAutoValidationActionFilter.cs
+++ FluentValidation.AutoValidation.Mvc/src/Filters/FluentValidationAutoValidationActionFilter.cs
@@ -54,10 +54,12 @@ namespace SharpGrip.FluentValidation.AutoValidation.Mvc.Filters
                         var parameterType = parameter.ParameterType;
                         var bindingSource = parameter.BindingInfo?.BindingSource;
 
-                        if (subject != null && parameterType.IsCustomType() &&
-                            ((autoValidationMvcConfiguration.EnableBodyBindingSourceAutomaticValidation && bindingSource == BindingSource.Body) ||
+                        if (subject != null 
+                         && parameterType.IsCustomType() 
+                         && ((autoValidationMvcConfiguration.EnableBodyBindingSourceAutomaticValidation && bindingSource == BindingSource.Body) ||
                              (autoValidationMvcConfiguration.EnableFormBindingSourceAutomaticValidation && bindingSource == BindingSource.Form) ||
-                             (autoValidationMvcConfiguration.EnableQueryBindingSourceAutomaticValidation && bindingSource == BindingSource.Query)))
+                             (autoValidationMvcConfiguration.EnableQueryBindingSourceAutomaticValidation && bindingSource == BindingSource.Query) || 
+                             (autoValidationMvcConfiguration.EnableCustomBindingSourceAutomaticValidation && bindingSource == BindingSource.Custom)))
                         {
                             if (serviceProvider.GetValidator(parameterType) is IValidator validator)
                             {
