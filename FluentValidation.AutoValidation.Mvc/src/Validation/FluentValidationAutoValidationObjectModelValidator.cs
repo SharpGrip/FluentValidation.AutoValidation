fd34ec617d893d0086194b00fd41fdfdca957e09
Rami <icnocop@users.noreply.github.com>
2025-09-09 20:23:56 -0700

    keeping existing format

--- FluentValidation.AutoValidation.Mvc/src/Validation/FluentValidationAutoValidationObjectModelValidator.cs
+++ FluentValidation.AutoValidation.Mvc/src/Validation/FluentValidationAutoValidationObjectModelValidator.cs
@@ -9,29 +9,19 @@ namespace SharpGrip.FluentValidation.AutoValidation.Mvc.Validation
     {
         private readonly bool disableBuiltInModelValidation;
 
-        public FluentValidationAutoValidationObjectModelValidator(
-            IModelMetadataProvider modelMetadataProvider,
-            IList<IModelValidatorProvider> validatorProviders,
-            bool disableBuiltInModelValidation)
+        public FluentValidationAutoValidationObjectModelValidator(IModelMetadataProvider modelMetadataProvider, IList<IModelValidatorProvider> validatorProviders, bool disableBuiltInModelValidation)
             : base(modelMetadataProvider, validatorProviders)
         {
             this.disableBuiltInModelValidation = disableBuiltInModelValidation;
         }
 
-        public override ValidationVisitor GetValidationVisitor(
-            ActionContext actionContext,
+        public override ValidationVisitor GetValidationVisitor(ActionContext actionContext,
             IModelValidatorProvider validatorProvider,
             ValidatorCache validatorCache,
             IModelMetadataProvider metadataProvider,
             ValidationStateDictionary? validationState)
         {
-            return new FluentValidationAutoValidationValidationVisitor(
-                actionContext,
-                validatorProvider,
-                validatorCache,
-                metadataProvider,
-                validationState,
-                disableBuiltInModelValidation);
+            return new FluentValidationAutoValidationValidationVisitor(actionContext, validatorProvider, validatorCache, metadataProvider, validationState, disableBuiltInModelValidation);
         }
     }
 }
